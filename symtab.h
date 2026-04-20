8ccb041945c20283845248b7a5854f314bbbefe3
minhera <120227761+minhera@users.noreply.github.com>
2026-04-19 23:12:54 -0700

    Update symtab.h

--- symtab.h
+++ symtab.h
@@ -2,35 +2,32 @@
 #define SYMTAB_H
 
 #include <string>
-#include <fstream> // Writing to file
 #include <unordered_map>
 #include <vector>
+
 using namespace std;
 
-struct SymtabInfo{
-    bool isCSect; // Check if the symbol is the control section (prints different stuff)
+struct SymtabInfo {
+    int address;
     bool isAbsolute;
-    int address; 
-    int length; // For Control Section print statement
 
-    // Constructors
-    SymtabInfo() : address(0), isCSect(false), length(0), isAbsolute(false) {}
-    SymtabInfo(int value, bool cs = false, bool absolute = false) : address(value), isCSect(cs), length(0), isAbsolute(absolute) {}
+    SymtabInfo(int addr = 0, bool abs = false)
+        : address(addr), isAbsolute(abs) {}
 };
 
-class SymbolTable{
+class SymbolTable {
 private:
     unordered_map<string, SymtabInfo> symTab;
     vector<string> symOrder;
+    int programLength;
+
 public:
-    SymbolTable() {};
+    SymbolTable();
 
-    // insert to table, look up symbol, write to file
-    bool insert_symbol(const string &symbol, int locctr, bool CS = false);
-    bool lookup_symbol(const string &name, SymtabInfo& symtab);
-    void set_length(const string &name, int length); // for END statement
-    void write_symtab(const string &filename, const string &CSName);
-
+    bool insert_symbol(const string& symbol, int locctr, bool CS = false);
+    bool lookup_symbol(const string& symbol, SymtabInfo& info) const;
+    void write_symtab(const string& filename, const string& CSName) const;
+    void set_length(int len);
 };
 
 #endif
